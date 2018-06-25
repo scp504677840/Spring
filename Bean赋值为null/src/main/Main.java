a3cf260af2b2dd6feb31d94bbb53346931d5f564
y1笑而过 <15335747148@163.com>
2018-06-26 00:55:47 +0800

    Bean赋值为null
    Bean赋值为null

---
+++ Bean赋值为null/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * IOC & DI
+ * <p>
+ * 1.ApplicationContext：面向开发者【开发使用】
+ * 2.BeanFactory：面向spring
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
+        Student outStudent = (Student) ctx.getBean("student");
+        System.out.println(outStudent);
+        //Student{name='null', age=0, height=0.0, book=null}
+    }
+}
