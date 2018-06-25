7effff95b4202a1752898e0efca6e448dbf1bf18
y1笑而过 <15335747148@163.com>
2018-06-26 01:28:55 +0800

    Bean集合属性Map
    Bean集合属性Map

---
+++ Bean集合属性Map/src/main/Student.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.util.List;
+import java.util.Map;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double height;
+    private Map<String,Book> books;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Map<String, Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Map<String, Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", books=" + books +
+                '}';
+    }
+
+}
