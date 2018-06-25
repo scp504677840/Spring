6008b317f293fc87a975fe878eebf16c8a082f91
y1笑而过 <15335747148@163.com>
2018-06-26 01:31:32 +0800

    Bean集合属性Set
    Bean集合属性Set

---
+++ Bean集合属性Set/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * IOC & DI
+ * <p>
+ * 1.ApplicationContext：面向开发者【开发使用】
+ * 2.BeanFactory：面向spring
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
+        Student outStudent = (Student) ctx.getBean("student");
+        System.out.println(outStudent);
+        //Student{name='Tom', age=22, height=180.0, books=[Book{name='Java', price=12.0}, Book{name='Go', price=25.0}, Book{name='Python', price=66.55}]}
+    }
+}
---
+++ Bean集合属性Set/src/main/Student.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double height;
+    private Set<Book> books;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", books=" + books +
+                '}';
+    }
+}
