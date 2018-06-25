ab71ea116f14f12f2363a24ec0344e7ed8221961
y1笑而过 <15335747148@163.com>
2018-06-26 01:16:56 +0800

    Bean集合属性List
    Bean集合属性List

---
+++ Bean集合属性List/src/main/Student.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.util.List;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double height;
+    private List<Book> books;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", books=" + books +
+                '}';
+    }
+}
