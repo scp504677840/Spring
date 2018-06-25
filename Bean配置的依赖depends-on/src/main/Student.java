8b4867a1b18b1b1e83dea008915fc1467a08087a
y1笑而过 <15335747148@163.com>
2018-06-26 02:44:08 +0800

    Bean配置的依赖depends-on
    Bean配置的依赖depends-on

---
+++ Bean配置的依赖depends-on/src/main/Student.java
@@ -0,0 +1,64 @@
+package main;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double height;
+    private Address address;
+    private Book book;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", address=" + address +
+                ", book=" + book +
+                '}';
+    }
+}
