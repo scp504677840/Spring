533e05154f0cbde2c8a03651f7085fb52b3d91a0
y1笑而过 <15335747148@163.com>
2018-06-26 22:03:43 +0800

    前置通知
    前置通知

--- AOP/前置通知/src/main/LogAspect.java
+++ AOP/前置通知/src/main/LogAspect.java
@@ -18,13 +18,15 @@ import java.util.List;
 public class LogAspect {
 
     //第一处省略：修饰符 + 【返回值类型】；其中返回值类型可以单独省略，但修饰符不行。例如：public *，但不可以：* double
-    //第二处省略：方法名称；
-    //第三处省略：参数类型列表；
+    //第二处省略：类名称；
+    //第三处省略：方法名称；
+    //第四处省略：参数类型列表；
     //@Before("execution(* main.ArithmeticCalculator.add(double ,double ))")
     //@Before("execution(* main.ArithmeticCalculator.*(double ,double ))")
     //@Before("execution(* main.ArithmeticCalculator.add(.. ))")
     //@Before("execution(* main.ArithmeticCalculator.*(double ,.. ))")
     //@Before("execution(* main.ArithmeticCalculator.*(.. ))")
+    //@Before("execution(* main.*.*(.. ))")
 
     /**
      * 前置通知：在目标方法开始前执行。
