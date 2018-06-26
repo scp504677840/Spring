0a81e2c0fcf7de8493e88fa51c3263ce7382bb0e
y1笑而过 <15335747148@163.com>
2018-06-26 16:27:12 +0800

    resource-pattern
    resource-pattern

---
+++ 通过注解配置Bean/resource-pattern/src/main/Main.java
@@ -0,0 +1,39 @@
+package main;
+
+import main.controller.BookController;
+import main.repository.BookRepository;
+import main.service.BookService;
+import main.entities.Book;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * IOC & DI
+ * <p>
+ * 1.ApplicationContext：面向开发者【开发使用】
+ * 2.BeanFactory：面向spring
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        BookController bookController = (BookController) ctx.getBean("bookController");
+        bookController.execute();
+
+//        BookService bookService = (BookService) ctx.getBean("bookService");
+//        bookService.save();
+
+//        BookRepository bookDao = (BookRepository) ctx.getBean("bookRepository");
+//        bookDao.save();
+
+//        Book book = (Book) ctx.getBean("book");
+//        System.out.println(book);
+
+        //BookController execute
+        //BookServiceImpl save
+        //BookRepositoryImpl save
+        //Book{name='null', price=0.0}
+    }
+
+}
